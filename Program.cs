761640be7b469c727e23011cf4d27512210dd69c
Irina <countess.all@yandex.ru>
2022-10-13 21:13:16 +0300

    вторая задача проекта

--- Program.cs
+++ Program.cs
@@ -19,3 +19,31 @@ Console.WriteLine("ЗАДАЧА 64");
 Console.Write("Введите натуральное число N ");
 int n = int.Parse(Console.ReadLine());
 Task64(n);
+Console.WriteLine();
+
+
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+*/
+
+int Task66(int M, int N)
+{
+    if (N == M)
+        return N;
+    return M + Task66(M+1, N);
+}
+
+Console.WriteLine("ЗАДАЧА 66");
+Console.Write("Введите концы промежутка натуральных числел M и N через пробел ");
+string[] inputdata = Console.ReadLine().Split();
+int a = int.Parse(inputdata[0]);
+int b = int.Parse(inputdata[1]);
+int s = Task66(a,b);
+Console.Write("сумму натуральных чисел в промежутке от " + a + " до " + b + " равна " + s);
+
+
+
+
+
